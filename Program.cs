243414d3037900d3286d69a0130928761a0754a9
Paige Rudnick <paige.rudnick@icloud.com>
2018-07-13 13:47:58 -0500

    Adding support for Attached Properties

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace dpGenerator
 {
+    using static SyntaxFactory;
+
     public class VariableResult
     {
         public int GroupIndex { get; set; }
@@ -15,8 +17,16 @@ namespace dpGenerator
 
     class Program
     {
-		private static readonly IdentifierNameSyntax getValue = SyntaxFactory.IdentifierName("GetValue");
-        private static readonly IdentifierNameSyntax setValue = SyntaxFactory.IdentifierName("SetValue");
+        [Flags]
+        enum DpOptions
+        {
+            None = 0,
+            RenderFrameworkCallback = (1 << 0),
+            RenderAttachedProperty = (2 << 1)
+        }
+
+		private static readonly IdentifierNameSyntax getValue = IdentifierName("GetValue");
+        private static readonly IdentifierNameSyntax setValue = IdentifierName("SetValue");
 			
         private static string ReadPostData()
         {
@@ -33,73 +43,109 @@ namespace dpGenerator
 
         private static PropertyDeclarationSyntax WrapProperty(TypeSyntax type, IdentifierNameSyntax dpName, VariableDeclaratorSyntax variable)
         {
-            return SyntaxFactory.PropertyDeclaration(type, variable.Identifier)
-                    .WithTrailingTrivia(SyntaxFactory.CarriageReturnLineFeed)
-                    .WithModifiers(SyntaxFactory.TokenList(SyntaxFactory.Token(SyntaxKind.PublicKeyword).WithTrailingTrivia(SyntaxFactory.Whitespace(" "))))
-                    .AddAccessorListAccessors(
-                        SyntaxFactory.AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
-                            .WithLeadingTrivia(SyntaxFactory.CarriageReturnLineFeed, SyntaxFactory.Tab)
-                            .WithBody(SyntaxFactory.Block(SyntaxFactory.ReturnStatement(SyntaxFactory.CastExpression(type.WithoutTrailingTrivia(), SyntaxFactory.InvocationExpression(getValue, SyntaxFactory.ArgumentList(SyntaxFactory.SingletonSeparatedList(SyntaxFactory.Argument(dpName)))))
-                                                                                                     .WithLeadingTrivia(SyntaxFactory.Whitespace(" ")))
-                                                                       .WithLeadingTrivia(SyntaxFactory.Whitespace(" "))
-                                                                       .WithTrailingTrivia(SyntaxFactory.Whitespace(" ")))),
-                        SyntaxFactory.AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
-                            .WithLeadingTrivia(SyntaxFactory.CarriageReturnLineFeed, SyntaxFactory.Tab)
-                            .WithBody(SyntaxFactory.Block(SyntaxFactory.ExpressionStatement(SyntaxFactory.InvocationExpression(setValue, SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(new ArgumentSyntax[] { SyntaxFactory.Argument(dpName), SyntaxFactory.Argument(SyntaxFactory.IdentifierName("value")).WithLeadingTrivia(SyntaxFactory.Whitespace(" ")) }))))
-                                                                       .WithLeadingTrivia(SyntaxFactory.Whitespace(" "))
-                                                                       .WithTrailingTrivia(SyntaxFactory.Whitespace(" "))))
-                            .WithTrailingTrivia(SyntaxFactory.CarriageReturnLineFeed));
+            return PropertyDeclaration(type, variable.Identifier)
+                .WithTrailingTrivia(CarriageReturnLineFeed)
+                .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword).WithTrailingTrivia(Whitespace(" "))))
+                .AddAccessorListAccessors(
+                    AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+                        .WithLeadingTrivia(CarriageReturnLineFeed, Tab)
+                        .WithBody(Block(ReturnStatement(CastExpression(type.WithoutTrailingTrivia(), InvocationExpression(getValue, ArgumentList(SingletonSeparatedList(Argument(dpName)))))
+                                                                                                    .WithLeadingTrivia(Whitespace(" ")))
+                                                                    .WithLeadingTrivia(Whitespace(" "))
+                                                                    .WithTrailingTrivia(Whitespace(" ")))),
+                    AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+                        .WithLeadingTrivia(CarriageReturnLineFeed, Tab)
+                        .WithBody(Block(ExpressionStatement(InvocationExpression(setValue, ArgumentList(SeparatedList(new ArgumentSyntax[] { Argument(dpName), Argument(IdentifierName("value")).WithLeadingTrivia(Whitespace(" ")) }))))
+                                                                    .WithLeadingTrivia(Whitespace(" "))
+                                                                    .WithTrailingTrivia(Whitespace(" "))))
+                        .WithTrailingTrivia(CarriageReturnLineFeed));
+        }
+
+        private static MethodDeclarationSyntax CreateAttachedSetter(TypeSyntax type, VariableDeclaratorSyntax variable)
+        {
+            var name = string.Concat("Set", variable.Identifier.ValueText);
+            return MethodDeclaration(PredefinedType(Token(SyntaxKind.VoidKeyword)), name)
+                .WithParameterList(ParameterList(SeparatedList(new[]
+                {
+                    Parameter(Identifier("element")).WithType(ParseTypeName("UIElement")),
+                    Parameter(Identifier("value")).WithType(type)
+                })))
+                .WithBody(Block(ExpressionStatement(
+                    InvocationExpression(MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, IdentifierName("element"), IdentifierName("SetValue")))
+                    .WithArgumentList(ArgumentList(SeparatedList(new[] 
+                    {
+                        Argument(IdentifierName(string.Concat(variable.Identifier.ValueText, "Property"))),
+                        Argument(IdentifierName("value")),
+                    }))))))
+                .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.StaticKeyword)))
+                .NormalizeWhitespace();
+        }
+
+        private static MethodDeclarationSyntax CreateAttachedGetter(TypeSyntax type, VariableDeclaratorSyntax variable)
+        {
+            var name = string.Concat("Get", variable.Identifier.ValueText);
+            return MethodDeclaration(type, name)
+                .WithParameterList(ParameterList(SeparatedList(new[]
+                {
+                    Parameter(Identifier("element")).WithType(ParseTypeName("UIElement"))
+                })))
+                .WithBody(Block(ReturnStatement(CastExpression(type, InvocationExpression(MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, IdentifierName("element"), IdentifierName("GetValue")))
+                    .WithArgumentList(ArgumentList(SeparatedList(new[]
+                    {
+                        Argument(IdentifierName(string.Concat(variable.Identifier.ValueText, "Property")))
+                    })))))))
+                .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.StaticKeyword)))
+                .NormalizeWhitespace();
         }
 
         private static MethodDeclarationSyntax AddChangedMethod(ClassDeclarationSyntax @class, VariableDeclaratorSyntax variable)
         {
             var variableName = string.Concat(@class.Identifier.Text[0].ToString().ToLowerInvariant(), @class.Identifier.Text.Substring(1));
-            return SyntaxFactory.MethodDeclaration(SyntaxFactory.PredefinedType(SyntaxFactory.Token(SyntaxKind.VoidKeyword)), GetCallbackMethodName(variable))
-                    .WithParameterList(SyntaxFactory.ParameterList(SyntaxFactory.SeparatedList(new[]
-                    {
-                        SyntaxFactory.Parameter(SyntaxFactory.Identifier("d")).WithType(SyntaxFactory.ParseTypeName("DependencyObject")),
-                        SyntaxFactory.Parameter(SyntaxFactory.Identifier("e")).WithType(SyntaxFactory.ParseTypeName("DependencyPropertyChangedEventArgs"))
-                    })))
-                    .WithBody(SyntaxFactory.Block(SyntaxFactory.LocalDeclarationStatement(SyntaxFactory.VariableDeclaration(SyntaxFactory.IdentifierName("var"))
-                        .WithVariables(SyntaxFactory.SeparatedList(new[]
-                    {
-                        SyntaxFactory.VariableDeclarator(variableName)
-                            .WithInitializer(SyntaxFactory.EqualsValueClause(SyntaxFactory.BinaryExpression(SyntaxKind.AsExpression, SyntaxFactory.IdentifierName("d"), SyntaxFactory.IdentifierName(@class.Identifier.Text))))
-                    }))), SyntaxFactory.IfStatement(SyntaxFactory.BinaryExpression(SyntaxKind.NotEqualsExpression, SyntaxFactory.IdentifierName(variableName), SyntaxFactory.LiteralExpression(SyntaxKind.NullLiteralExpression)), SyntaxFactory.Block())))
-                    .WithModifiers(SyntaxFactory.TokenList(SyntaxFactory.Token(SyntaxKind.PrivateKeyword), SyntaxFactory.Token(SyntaxKind.StaticKeyword)))
-                    .NormalizeWhitespace();
+            return MethodDeclaration(PredefinedType(Token(SyntaxKind.VoidKeyword)), GetCallbackMethodName(variable))
+                .WithParameterList(ParameterList(SeparatedList(new[]
+                {
+                    Parameter(Identifier("d")).WithType(ParseTypeName("DependencyObject")),
+                    Parameter(Identifier("e")).WithType(ParseTypeName("DependencyPropertyChangedEventArgs"))
+                })))
+                .WithBody(Block(LocalDeclarationStatement(VariableDeclaration(IdentifierName("var"))
+                    .WithVariables(SeparatedList(new[]
+                {
+                    VariableDeclarator(variableName)
+                        .WithInitializer(EqualsValueClause(BinaryExpression(SyntaxKind.AsExpression, IdentifierName("d"), IdentifierName(@class.Identifier.Text))))
+                }))), IfStatement(BinaryExpression(SyntaxKind.NotEqualsExpression, IdentifierName(variableName), LiteralExpression(SyntaxKind.NullLiteralExpression)), Block())))
+                .WithModifiers(TokenList(Token(SyntaxKind.PrivateKeyword), Token(SyntaxKind.StaticKeyword)))
+                .NormalizeWhitespace();
         }
 
-        private static FieldDeclarationSyntax AddDependencyProperty(TypeSyntax type, IdentifierNameSyntax dpName, ClassDeclarationSyntax @class, VariableDeclaratorSyntax variable, bool renderFrameworkCallback)
+        private static FieldDeclarationSyntax AddDependencyProperty(TypeSyntax type, IdentifierNameSyntax dpName, ClassDeclarationSyntax @class, VariableDeclaratorSyntax variable, DpOptions options)
         {            
-            var memberaccess = SyntaxFactory.MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, SyntaxFactory.IdentifierName("DependencyProperty"), SyntaxFactory.IdentifierName("Register"));
+            var memberaccess = MemberAccessExpression(SyntaxKind.SimpleMemberAccessExpression, IdentifierName("DependencyProperty"), IdentifierName((options & DpOptions.RenderAttachedProperty) == DpOptions.RenderAttachedProperty ? "RegisterAttached" : "Register"));
 
-            var frameworkMetadataArguments = new ArgumentSyntax[] { SyntaxFactory.Argument(variable.Initializer?.Value ?? SyntaxFactory.DefaultExpression(type)) };
-            if (renderFrameworkCallback)
-                frameworkMetadataArguments = frameworkMetadataArguments.Concat(new []{ SyntaxFactory.Argument(SyntaxFactory.IdentifierName(GetCallbackMethodName(variable))) }).ToArray();
+            var frameworkMetadataArguments = new ArgumentSyntax[] { Argument(variable.Initializer?.Value ?? DefaultExpression(type)) };
+            if ((options & DpOptions.RenderFrameworkCallback) == DpOptions.RenderFrameworkCallback)
+                frameworkMetadataArguments = frameworkMetadataArguments.Concat(new []{ Argument(IdentifierName(GetCallbackMethodName(variable))) }).ToArray();
 
-            var argumentList = SyntaxFactory.SeparatedList(new[]
+            var argumentList = SeparatedList(new[]
             {
-                SyntaxFactory.Argument(SyntaxFactory.InvocationExpression(SyntaxFactory.IdentifierName("nameof")).WithArgumentList(SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(new[] { SyntaxFactory.Argument(SyntaxFactory.IdentifierName(variable.Identifier.Text))})))),
-                SyntaxFactory.Argument(SyntaxFactory.TypeOfExpression(type)),
-                SyntaxFactory.Argument(SyntaxFactory.TypeOfExpression(SyntaxFactory.ParseTypeName(@class.Identifier.Text))),
-                SyntaxFactory.Argument(SyntaxFactory.ObjectCreationExpression(SyntaxFactory.ParseTypeName("FrameworkPropertyMetadata")).WithArgumentList(SyntaxFactory.ArgumentList(SyntaxFactory.SeparatedList(frameworkMetadataArguments))))
+                (options & DpOptions.RenderAttachedProperty) == DpOptions.RenderAttachedProperty 
+                    ? Argument(LiteralExpression(SyntaxKind.StringLiteralExpression, Literal(variable.Identifier.Text))) : 
+                    Argument(InvocationExpression(IdentifierName("nameof")).WithArgumentList(ArgumentList(SeparatedList(new[] { Argument(IdentifierName(variable.Identifier.Text))})))),
+                Argument(TypeOfExpression(type)),
+                Argument(TypeOfExpression(ParseTypeName(@class.Identifier.Text))),
+                Argument(ObjectCreationExpression(ParseTypeName("FrameworkPropertyMetadata")).WithArgumentList(ArgumentList(SeparatedList(frameworkMetadataArguments))))
             });
 
-            var registerCall =
-                SyntaxFactory.ExpressionStatement(
-                SyntaxFactory.InvocationExpression(memberaccess,
-                SyntaxFactory.ArgumentList(argumentList)));
+            var registerCall = ExpressionStatement(InvocationExpression(memberaccess, ArgumentList(argumentList)));
 
-            return SyntaxFactory.FieldDeclaration(
-                SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName("DependencyProperty"))
+            return FieldDeclaration(
+                VariableDeclaration(ParseTypeName("DependencyProperty"))
                 .WithVariables(
-                    SyntaxFactory.SingletonSeparatedList(
-                        SyntaxFactory.VariableDeclarator(
-                            SyntaxFactory.Identifier(dpName.Identifier.Text))
-                                .WithInitializer(SyntaxFactory.EqualsValueClause(SyntaxFactory.InvocationExpression(memberaccess, SyntaxFactory.ArgumentList(argumentList))
+                    SingletonSeparatedList(
+                        VariableDeclarator(
+                            Identifier(dpName.Identifier.Text))
+                                .WithInitializer(EqualsValueClause(InvocationExpression(memberaccess, ArgumentList(argumentList))
                 )))))
-            .WithModifiers(SyntaxFactory.TokenList(SyntaxFactory.Token(SyntaxKind.PublicKeyword), SyntaxFactory.Token(SyntaxKind.StaticKeyword), SyntaxFactory.Token(SyntaxKind.ReadOnlyKeyword)))
+            .WithModifiers(TokenList(Token(SyntaxKind.PublicKeyword), Token(SyntaxKind.StaticKeyword), Token(SyntaxKind.ReadOnlyKeyword)))
             .NormalizeWhitespace();
         }
 
@@ -114,11 +160,14 @@ namespace dpGenerator
         {
             foreach (var variable in variableDeclaration.Variables)
             {
-                var dpName = SyntaxFactory.IdentifierName(string.Concat(variable.Identifier.Text, "Property"));
-                yield return new VariableResult() { GroupIndex = 0, Value = (SyntaxNode)WrapProperty(variableDeclaration.Type, dpName, variable) };
-                yield return new VariableResult() { GroupIndex = 1, Value = (SyntaxNode)AddChangedMethod(@class, variable) };
-                yield return new VariableResult() { GroupIndex = 2, Value = (SyntaxNode)AddDependencyProperty(variableDeclaration.Type, dpName, @class, variable, true) };
-                yield return new VariableResult() { GroupIndex = 3, Value = (SyntaxNode)AddDependencyProperty(variableDeclaration.Type, dpName, @class, variable, false) };
+                var dpName = IdentifierName(string.Concat(variable.Identifier.Text, "Property"));
+                yield return new VariableResult() { GroupIndex = 0, Value = WrapProperty(variableDeclaration.Type, dpName, variable) };
+                yield return new VariableResult() { GroupIndex = 1, Value = AddChangedMethod(@class, variable) };
+                yield return new VariableResult() { GroupIndex = 2, Value = CreateAttachedSetter(variableDeclaration.Type, variable) };
+                yield return new VariableResult() { GroupIndex = 3, Value = CreateAttachedGetter(variableDeclaration.Type, variable) };
+                yield return new VariableResult() { GroupIndex = 4, Value = AddDependencyProperty(variableDeclaration.Type, dpName, @class, variable, DpOptions.RenderFrameworkCallback) };
+                yield return new VariableResult() { GroupIndex = 5, Value = AddDependencyProperty(variableDeclaration.Type, dpName, @class, variable, DpOptions.None) };
+                yield return new VariableResult() { GroupIndex = 6, Value = AddDependencyProperty(variableDeclaration.Type, dpName, @class, variable, DpOptions.RenderAttachedProperty) };
             }
         }
 
@@ -130,13 +179,13 @@ namespace dpGenerator
 
             foreach (var group in results.GroupBy(r => r.GroupIndex))
             {
-                if(group.Key >= 3)
+                if(group.Key >= 4)
                     Console.WriteLine();
 
                 foreach (var item in group)
                 {
                     Console.WriteLine(item.Value);
-                    if (group.Key < 2)
+                    if (group.Key < 4)
                         Console.WriteLine();
                 }
             }
